5d9ca2050794295ad49efda83dab35ad5b782b34
DamienDeSousa <desousadamien30@gmail.com>
2023-08-19 19:04:05 +0200

    fix url social media validator

--- services/socialMedias/socialMedias.validators.ts
+++ services/socialMedias/socialMedias.validators.ts
@@ -1,14 +1,14 @@
 export const isFacebookProfileLink = (link: string): boolean => {
-  const regex = /^https:\/\/www\.facebook\.com\//
+  const regex = /^https?:\/\/(?:www\.)?facebook\.com\//
   return regex.test(link)
 }
 
 export const isInstagramProfileLink = (link: string): boolean => {
-  const regex = /^https:\/\/www\.instagram\.com\//
+  const regex = /^https?:\/\/(?:www\.)?instagram\.com\//
   return regex.test(link)
 }
 
 export const isTwitterProfileLink = (link: string): boolean => {
-  const regex = /^https:\/\/www\.twitter\.com\//
+  const regex = /^https?:\/\/(?:www\.)?twitter\.com\//
   return regex.test(link)
 }
